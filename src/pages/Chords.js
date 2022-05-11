8f8b7ad5d158053b56e25a08b1321bece6b6510e
Chris Goggans <cgogg1@gmail.com>
2022-05-10 20:53:03 -0400

    finished functionality of intervals page

--- src/pages/Chords.js
+++ src/pages/Chords.js
@@ -1,49 +1,98 @@
-import * as Tone from "tone";
-import React, { useState } from "react";
+import '../App.css';
+import * as Tone from 'tone'
+import { AMSynth } from 'tone';
+import { useState } from 'react'
 import Button from "@mui/material/Button";
-import { Stack } from "@mui/material";
 
-// const synth = new Tone.Synth().toDestination();
+const answerObj = { chordAnswer: [], correctAns: "" }
 
-const synth = new Tone.PolySynth().toDestination();
-// set the attributes across all the voices using 'set'
-synth.set({ detune: -1200 });
-// play a chord
-//randomizing function for chords
-
-
-
+const scoreObj = { score: 0, totalQs: 0 }
 
 function Chords() {
-  // let [tone, setTone] = useState([]);
-  let tone =[ ["D4", "F#4", "A5"], ["D4", "F4", "A5"], ["D4", "F#4", "A#5"], ["D4", "F4", "Ab5"], ["C4", "E4", "G4"], ["C4", "Eb4", "G4"], ["C4", "Eb4", "Gb4"], ["C4", "E4", "G#4"] ]
-  
-  tone = tone.sort(() => Math.random() - 0.5);
-  
-  function playSynth() {
-    synth.triggerAttackRelease(tone[0], 1);
-    console.log(tone)
-  } 
- 
+  const [answer, setAnswer] = useState(answerObj)
+  const [scoreboard, setScoreboard] = useState(scoreObj)
+
+  const synth = new Tone.PolySynth(AMSynth).toDestination()
+
+  const notes = [ "C", "C#", "D", "D#", "E", "F", "F#", "G", "Ab", "A", "Bb", "B" ]
+  let randomNote = notes[Math.floor(Math.random() * notes.length)]
+
+  const chords = {
+    major: Tone.Frequency(`${randomNote}3`).harmonize([0, 4, 7]), // major
+    minor: Tone.Frequency(`${randomNote}3`).harmonize([0, 3, 7]), // minor
+    augmented: Tone.Frequency(`${randomNote}3`).harmonize([0, 4, 8]), // augmented
+    diminished_triad: Tone.Frequency(`${randomNote}3`).harmonize([0, 3, 6]), // diminished
+    major_7th: Tone.Frequency(`${randomNote}3`).harmonize([0, 4, 7, 11]), // major 7th
+    dominant_7th: Tone.Frequency(`${randomNote}3`).harmonize([0, 4, 7, 10]), // dominant 7th
+    minor_7th: Tone.Frequency(`${randomNote}3`).harmonize([0, 3, 7, 10]), // minor 7th
+    diminished: Tone.Frequency(`${randomNote}3`).harmonize([0, 3, 6, 9]), // diminished
+    half_diminished: Tone.Frequency(`${randomNote}3`).harmonize([0, 3, 6, 10]) // half diminished 
+  }
+
+  const chordsKeys = Object.keys(chords)
+
+  console.log(Tone.context.state)
+
+  function playSound() {
+    const chordQuality = chordsKeys[Math.floor(Math.random() * chordsKeys.length)]
+
+    console.log(chordQuality)
+    console.log(randomNote)
+   
+    let currentChord = chords[chordQuality]
+
+    synth.triggerAttackRelease(currentChord, "2n")
+    Tone.start()
+
+    setAnswer({ chord: currentChord, correctAns: chordQuality})
+    setScoreboard(prev => ({...scoreboard, 
+      totalQs:prev.totalQs+1}))
+    console.log(Tone.context.state)
+  }
+
+  function playSoundAgain() {
+    synth.triggerAttackRelease(answer.chord, "2n")
+    Tone.start()
+  }
+
+  function chordSelection({target: {value}}) {
+    if (value === answer.correctAns){
+      setScoreboard(prev => ({...scoreboard, score:prev.score+1}))}
+    else
+      alert("INCORRECT")
+  }
+
   return (
-    <>
-    <div className="playChordButton">
-      <Button variant="outlined" onClick={playSynth}>
-            Play Chord
-      </Button>
+    <div className="pageContainer">
+      
+      <div className='scoreBoard'>
+        <h3>Score: {scoreboard.score} out of {scoreboard.totalQs}</h3>
+      </div>
+            
+      <div className='playButtons'>
+        <Button variant="outlined" className="the-one-button" onClick={playSound}>play me</Button>
+      </div> 
+
+      <div className='answerOptions'>
+        <h2>What chord just played??</h2>
+        <div className='form'>
+          {chordsKeys.map(chord => {
+            const correctedName = chord.split('_')
+             const corrected = correctedName[0][0].toUpperCase() + correctedName[0].substring(1) + " " + (correctedName.length === 2? correctedName[1][0].toUpperCase() + correctedName[1].substring(1) : "")
+                
+              return <Button variant="outlined" className='button' 
+                  key={chord}  
+                  value={chord} name={chord} 
+                  onClick={chordSelection}>{corrected}</Button>
+            })
+          }
+          </div>
+      </div>
+    
+      <div className='playAgainButton'>
+        <Button variant="outlined" className='button' onClick={playSoundAgain}>One more time pls</Button>
+      </div>
     </div>
-    <div className="chords-wrapper">
-      <Stack className="chord-btns" spacing={2} direction="row">
-        
-        
-        
-        <Button className="btn-mobile" variant="outlined">Major</Button>
-        <Button className="btn-mobile" variant="outlined">Minor</Button>
-        <Button className="btn-mobile" variant="outlined">Diminished</Button>
-        <Button className="btn-mobile" variant="outlined">Augmented</Button>
-      </Stack>
-    </div>
-    </>
   );
 }
 
--- src/pages/Intervals.js
+++ src/pages/Intervals.js
@@ -1,71 +1,158 @@
 import * as Tone from "tone";
-
+import { useState } from "react";
 import Button from "@mui/material/Button";
-import { Stack } from "@mui/material";
+
+const answerObj = { chordAnswer: [], correctAns: "" };
+
+const scoreObj = { score: 0, totalQs: 0 };
 
 function Intervals() {
-    const synth = new Tone.Synth().toDestination();
-    // use an array of objects as long as the object has a "time" attribute
-    const part = new Tone.Part(((time, value) => {
-        // the value is an object which contains both the note and the velocity
-        synth.triggerAttackRelease(value.note, "8n", time, value.velocity);
-    }), 
-    
-    [
-        { 
-            time: 0, 
-            note: "G5", 
-            velocity: 0.9 
-        },
-    { 
-        time: "0:2", 
-        note: "D6", 
-        velocity: 0.5 
-    }
-],
-[{ 
-    time: 0, 
-    note: "C3", 
-    velocity: 0.9 
-},
-    { 
-        time: "0:2", 
-        note: "C4", 
-        velocity: 0.5 
-    }
-], ).start(0);
-   function playInterval(){
-    Tone.Transport.start();
-    console.log("im working")
-   
-   }
-        
-    
-    
+  const [answer, setAnswer] = useState(answerObj);
+  const [scoreboard, setScoreboard] = useState(scoreObj);
 
-    return (
-        <>
-        <div className="playChordButton">
-          <Button onClick={playInterval} variant="outlined" >
-                Play Inverval
-          </Button>
-        </div>
-        <div className="chords-wrapper">
-          <Stack className="chord-btns" spacing={2} direction="row">
-            
-            
-            
-            <Button className="btn-mobile" variant="outlined">m2</Button>
-            <Button className="btn-mobile" variant="outlined">M2</Button>
-            <Button className="btn-mobile" variant="outlined">m3</Button>
-            <Button className="btn-mobile" variant="outlined">M3</Button>
-            <Button className="btn-mobile" variant="outlined">P4</Button>
-            <Button className="btn-mobile" variant="outlined">P5</Button>
-          </Stack>
-        </div>
-        </>
-      );
+  const synth = new Tone.Synth().toDestination();
+  // use an array of objects as long as the object has a "time" attribute
 
+  const notes = [
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "Ab",
+    "A",
+    "Bb",
+    "B",
+  ];
+  let randomNote = notes[Math.floor(Math.random() * notes.length)];
+
+  const intervals = {
+    minor2: Tone.Frequency(`${randomNote}3`).transpose(1),
+    Major2: Tone.Frequency(`${randomNote}3`).transpose(2),
+    minor3: Tone.Frequency(`${randomNote}3`).transpose(3),
+    Major3: Tone.Frequency(`${randomNote}3`).transpose(4),
+    P4: Tone.Frequency(`${randomNote}3`).transpose(5),
+    P5: Tone.Frequency(`${randomNote}3`).transpose(6),
+    minor6: Tone.Frequency(`${randomNote}3`).transpose(7),
+    Major6: Tone.Frequency(`${randomNote}3`).transpose(8),
+    minor7: Tone.Frequency(`${randomNote}3`).transpose(9),
+    Major7: Tone.Frequency(`${randomNote}3`).transpose(10),
+    Unison: Tone.Frequency(`${randomNote}3`).transpose(11),
+  };
+
+  const intervalKeys = Object.keys(intervals);
+
+  console.log(Tone.context.state);
+
+  function playSound() {
+    const intervalQuality =
+      intervalKeys[Math.floor(Math.random() * intervalKeys.length)];
+
+    console.log(intervalQuality);
+    console.log(randomNote);
+
+    let currentInterval = intervals[intervalQuality];
+
+    synth.triggerAttackRelease(currentInterval, "2n");
+    Tone.start();
+
+    setAnswer({ interval: currentInterval, correctAns: intervalQuality });
+    setScoreboard((prev) => ({ ...scoreboard, totalQs: prev.totalQs + 1 }));
+    console.log(Tone.context.state);
+  }
+
+  // const part = new Tone
+  //    function playInterval(){
+  //     Tone.Transport.start();
+  //     console.log("im working")
+  //     console.log(part);
+
+  //    }
+  function playTonic() {
+    synth.triggerAttackRelease(`${randomNote}3`, "8n");
+  }
+
+  function playSoundAgain() {
+    synth.triggerAttackRelease(answer.interval, "2n");
+    Tone.start();
+  }
+
+  function intervalSelection({ target: { value } }) {
+    if (value === answer.correctAns) {
+      setScoreboard((prev) => ({ ...scoreboard, score: prev.score + 1 }));
+    } else alert("INCORRECT");
+  }
+
+  return (
+    <div className="pageContainer">
+      <div className="scoreBoard">
+        <h3>
+          Score: {scoreboard.score} out of {scoreboard.totalQs}
+        </h3>
+      </div>
+
+      <div className="playTonic">
+        <Button
+          variant="outlined"
+          className="the-one-button"
+          onClick={playTonic}
+        >
+          Tonic
+        </Button>
+      </div>
+
+      <div className="playButtons">
+        <Button
+          variant="outlined"
+          className="the-one-button"
+          onClick={playSound}
+        >
+          Ascending Interval
+        </Button>
+      </div>
+
+      <div className="answerOptions">
+        <h2>What interval just played??</h2>
+        <div className="form">
+          {intervalKeys.map((interval) => {
+            const correctedName = interval.split("_");
+            const corrected =
+              correctedName[0][0].toUpperCase() +
+              correctedName[0].substring(1) +
+              " " +
+              (correctedName.length === 2
+                ? correctedName[1][0].toUpperCase() +
+                  correctedName[1].substring(1)
+                : "");
+
+            return (
+              <Button
+                variant="outlined"
+                className="button"
+                key={interval}
+                value={interval}
+                name={interval}
+                onClick={intervalSelection}
+              >
+                {corrected}
+              </Button>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="playAgainButton">
+        <Button variant="outlined" className="button" onClick={playSoundAgain}>
+          One more time pls
+        </Button>
+      </div>
+    </div>
+  );
 }
+//
 
 export default Intervals;
+
